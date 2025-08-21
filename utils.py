61110b75156345090b457703bef5411e0fbf512b
Ava Gordon <avaamgordonn@gmail.com>
2025-08-20 21:43:29 -0400

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,53 @@
+import torch
+import numpy as np
+import pandas as pd
+import os
+
+
+def pdist_torch(emb1, emb2):
+    '''
+    compute the eucilidean distance matrix between embeddings1 and embeddings2
+    using gpu
+    '''
+    m, n = emb1.shape[0], emb2.shape[0]
+    emb1_pow = torch.pow(emb1, 2).sum(dim = 1, keepdim = True).expand(m, n)
+    emb2_pow = torch.pow(emb2, 2).sum(dim = 1, keepdim = True).expand(n, m).t()
+    dist_mtx = emb1_pow + emb2_pow
+    dist_mtx = dist_mtx.addmm_(1, -2, emb1, emb2.t())
+    dist_mtx = dist_mtx.clamp(min = 1e-12).sqrt()
+    return dist_mtx
+
+
+def pdist_np(emb1, emb2):
+    '''
+    compute the eucilidean distance matrix between embeddings1 and embeddings2
+    using cpu
+    '''
+    m, n = emb1.shape[0], emb2.shape[0]
+    emb1_pow = np.square(emb1).sum(axis = 1)[..., np.newaxis]
+    emb2_pow = np.square(emb2).sum(axis = 1)[np.newaxis, ...]
+    dist_mtx = -2 * np.matmul(emb1, emb2.T) + emb1_pow + emb2_pow
+    dist_mtx = np.sqrt(dist_mtx.clip(min = 1e-12))
+    return dist_mtx
+
+def check_dataset():
+
+    metadata = pd.read_csv('datasets/animal-clef-2025/metadata_small.csv')
+
+    for _, row in metadata.iterrows():
+        if not os.path.exists('datasets/animal-clef-2025/' + row['path']):
+            print(f"File does not exist. {row['path']}")
+
+def remove_augmented_files(root_dir):
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if '_augmented' in filename:
+                file_path = os.path.join(dirpath, filename)
+                try:
+                    os.remove(file_path)
+                    print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Failed to delete {file_path}: {e}")
+
+if __name__ == '__main__':
+    remove_augmented_files("./datasets")
